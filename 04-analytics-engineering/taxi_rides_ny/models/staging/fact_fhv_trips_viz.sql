8396930a1e4612397388c6ceaf4aff55f0f3d704
Abdimalik Uali <abdimalikoffking@gmail.com>
2024-04-05 00:34:10 +0000

    delete 2

--- 04-analytics-engineering/taxi_rides_ny/models/staging/fact_fhv_trips_viz.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE OR REPLACE VIEW `taxi-nyc-419320.dbt_u_malik.fact_fhv_trips_viz` AS (
-    SELECT
-        EXTRACT(MONTH FROM pickup_datetime) AS trip_month,
-        COUNT(*) AS trip_count
-    FROM fact_fhv_trips
-    GROUP BY trip_month
-    ORDER BY trip_month
-);
